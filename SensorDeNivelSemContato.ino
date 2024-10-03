f745def8f8577cfe6a9b51c13f9a9a0c28b0785c
Iori Fernando <97190926+IoriFernando@users.noreply.github.com>
2024-10-03 17:32:26 -0300

    Upload do código do sensor de nível

---
+++ SensorDeNivelSemContato.ino
@@ -0,0 +1,19 @@
+const int sensorPin = 2;  // Pino conectado à saída do sensor
+int sensorState = 0;      // Variável para armazenar o estado do sensor
+
+void setup() {
+    Serial.begin(9600);          // Inicializa a comunicação serial
+    pinMode(sensorPin, INPUT);   // Configura o pino do sensor como entrada
+}
+
+void loop() {
+    sensorState = digitalRead(sensorPin);  // Lê o estado do sensor
+
+    if (sensorState == HIGH) {
+        Serial.println("Liquido detectado!"); // O nível de líquido foi detectado
+    } else {
+        Serial.println("Liquido não detectado."); // Nenhum líquido detectado
+    }
+
+    delay(1000);  // Aguarda meio segundo antes de ler novamente
+}
